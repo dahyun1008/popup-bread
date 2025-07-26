006d88c01311c7eb2791b75c7d840400c970bf3b
김다현 <dahyun@dahyun-MacBookPro.local>
2025-07-26 11:37:22 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/bread/popupbread/PopupBreadApplication.java
@@ -0,0 +1,13 @@
+package com.bread.popupbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupBreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopupBreadApplication.class, args);
+    }
+
+}
