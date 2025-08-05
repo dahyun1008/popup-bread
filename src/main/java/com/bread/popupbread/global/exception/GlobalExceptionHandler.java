cbd369570a55d05ff287a57720b433f3667fa217
dahyun1008 <gimd15629@gmail.com>
2025-08-05 17:55:53 +0900

    feat: 유저 인증 예외 핸들러 정의

--- src/main/java/com/bread/popupbread/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bread/popupbread/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,35 @@
 package com.bread.popupbread.global.exception;
 
+import com.bread.popupbread.global.exception.auth.AuthServiceException;
+import com.bread.popupbread.global.exception.auth.IllegalCodeException;
+import com.bread.popupbread.global.exception.auth.MissingCodeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.net.URI;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    private ResponseEntity<Void> redirectToLoginWithError(String errorCode){
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("/login?error=" + errorCode))
+                .build();
+    }
+
+    @ExceptionHandler(MissingCodeException.class)
+    public ResponseEntity<Void> handleMissingCodeException() {
+        return redirectToLoginWithError("missing_code");
+    }
+
+    @ExceptionHandler(IllegalCodeException.class)
+    public ResponseEntity<Void> handleIllegalCodeException() {
+        return redirectToLoginWithError("illegal_code");
+    }
+
+    @ExceptionHandler(AuthServiceException.class)
+    public ResponseEntity<Void> handleAuthServiceException() {
+        return redirectToLoginWithError("auth_service");
+    }
 }
