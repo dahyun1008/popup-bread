25361c4ecc5f99121d757498beb849b5b58d6c00
dahyun1008 <gimd15629@gmail.com>
2025-08-05 15:04:30 +0900

    test: 유저 도메인 통합 테스트 작성

--- src/test/java/com/bread/popupbread/integration/user/UserIntegrationTest.java
+++ src/test/java/com/bread/popupbread/integration/user/UserIntegrationTest.java
@@ -1,4 +1,35 @@
 package com.bread.popupbread.integration.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.yml")
+@Transactional
 public class UserIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 카카오_로그인_성공시_JWT_토근_반환됨() throws Exception {
+        // given
+        String kakaoAccessToken = "dummy_kakao_token";
+
+        // when & then
+        mockMvc.perform(post("/api/v1/auth/kakao")
+                .header("Authorization", "Bearer " + kakaoAccessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").exists());
+    }
 }
