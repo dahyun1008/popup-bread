fb923d6e73f8e96faf2d82cb3367400c41cc38fd
dahyun1008 <gimd15629@gmail.com>
2025-08-05 15:37:19 +0900

    feat: kakao 설정 주입

--- src/main/java/com/bread/popupbread/global/config/properties/KakaoProperties.java
+++ src/main/java/com/bread/popupbread/global/config/properties/KakaoProperties.java
@@ -1,4 +1,15 @@
 package com.bread.popupbread.global.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "kakao")
 public class KakaoProperties {
+    private String clientId;
+    private String redirectUri;
 }
