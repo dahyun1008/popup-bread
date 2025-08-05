182e69133caa66d401b2eada20bf529641b73a7d
dahyun1008 <gimd15629@gmail.com>
2025-08-05 15:37:02 +0900

    feat: jwt 토큰 설정 주입

--- src/main/java/com/bread/popupbread/global/config/properties/JwtProperties.java
+++ src/main/java/com/bread/popupbread/global/config/properties/JwtProperties.java
@@ -1,4 +1,15 @@
 package com.bread.popupbread.global.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+    private String secret;
+    private long expirationMs;
 }
