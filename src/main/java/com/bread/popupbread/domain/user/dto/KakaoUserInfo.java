805d25b456c8cf9e8b9751b6cc38b439123bfd17
dahyun1008 <gimd15629@gmail.com>
2025-08-05 15:31:27 +0900

    feat: kakao 유저 정보 저장용 내부 dto

--- src/main/java/com/bread/popupbread/domain/user/dto/KakaoUserInfo.java
+++ src/main/java/com/bread/popupbread/domain/user/dto/KakaoUserInfo.java
@@ -1,4 +1,14 @@
 package com.bread.popupbread.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class KakaoUserInfo {
+    private Long kakaoId;
+    private String nickname;
+    private String email;
 }
