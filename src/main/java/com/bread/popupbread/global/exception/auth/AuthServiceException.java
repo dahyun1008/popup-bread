34a1ea7405101827240c8bf746bd74d86d62e3a8
dahyun1008 <gimd15629@gmail.com>
2025-08-05 17:54:46 +0900

    feat: 유저 인증 에러 커스텀 예외 클래스

--- src/main/java/com/bread/popupbread/global/exception/auth/AuthServiceException.java
+++ src/main/java/com/bread/popupbread/global/exception/auth/AuthServiceException.java
@@ -1,4 +1,7 @@
 package com.bread.popupbread.global.exception.auth;
 
-public class AuthServiceException {
+public class AuthServiceException extends RuntimeException{
+    public AuthServiceException(String message) {
+        super(message);
+    }
 }
