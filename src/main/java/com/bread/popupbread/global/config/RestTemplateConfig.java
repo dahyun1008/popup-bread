e0637d4c68ef69e8766c8a85e70dc0aa971ab009
dahyun1008 <gimd15629@gmail.com>
2025-10-01 20:22:43 +0900

    config: RestTemplate 설정 추가

---
+++ src/main/java/com/bread/popupbread/global/config/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package com.bread.popupbread.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000); // ms
+        factory.setReadTimeout(3000);    // ms
+        return new RestTemplate(factory);
+    }
+}
